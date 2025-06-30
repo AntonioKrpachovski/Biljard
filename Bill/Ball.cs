51f0c34e013de32af0f4753dcafc4dfedcc0e821
AntonioKrpachovski <krpacovski.antonio@gmail.com>
2025-07-01 00:30:43 +0200

    Почетна верзија на проектот

---
+++ Bill/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bill
+{
+    public class Ball
+    {
+        public int ballX { get; set; }
+        public int ballY { get; set; }
+        
+        public string Number { get; set; }
+        public Color BaseColor { get; set; }
+        public int radius = 12;
+        public float VelocityX = 0;
+        public float VelocityY = 0;
+
+        public Ball(int ballX, int ballY, string number, Color baseColor)
+        {
+            this.ballX = ballX;
+            this.ballY = ballY;
+            Number = number;
+            BaseColor = baseColor;
+        }
+        public void Draw(Graphics g) {
+            Brush b = new SolidBrush(BaseColor);
+            Brush bString = new SolidBrush(Color.Black);
+            Font f = new Font("Arial", (float)(radius * 0.9), FontStyle.Bold, GraphicsUnit.Pixel);
+            g.FillEllipse(b, ballX - radius, ballY - radius, 2 * radius, 2 * radius);
+
+
+            // Calculate position to center the text (so chatgpt e napraveno centriranje na textot vo topkite)
+
+            SizeF textSize = g.MeasureString(Number, f);
+
+            
+            float textX = ballX - textSize.Width / 2;
+            float textY = ballY - textSize.Height / 2;
+
+            g.DrawString(Number, f, bString, textX, textY);
+
+        }
+    }
+}
---
+++ Bill/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bill
+{
+    public partial class Form1 : Form
+    {
+        public List<Ball> balls;
+        public Form1()
+        {
+            InitializeComponent();
+            balls = new List<Ball>();
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            balls.Add(new Ball(e.Location.X, e.Location.Y, "10", Color.Green));
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Ball ball in balls) {
+                ball.Draw(e.Graphics);
+            }
+        }
+    }
+}
